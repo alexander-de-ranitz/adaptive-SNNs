1eca66ff480012984ddd74ce4a853a31dd2d3165
Alexander de Ranitz <alexanderderanitz@gmail.com>
2025-09-24 11:54:09 +0200

    Add spiking to run function, pass model instead of terms

--- src/adaptive_SNN/utils/solver.py
+++ src/adaptive_SNN/utils/solver.py
@@ -9,19 +9,21 @@ from diffrax import Euler
 from typing import Tuple
 from diffrax import RESULTS, AbstractTerm, Solution
 from typing_extensions import TypeAlias
+from adaptive_SNN.models.models import NoisyNeuronModel
 
 
 _ErrorEstimate: TypeAlias  = None
 _SolverState: TypeAlias = None
 
-def run_SNN_simulation(terms: AbstractTerm, solver: Euler, t0: float, t1: float, dt0: float, y0: PyTree, save_every_n_steps: int = 1, args: PyTree = None):
+def run_SNN_simulation(model: NoisyNeuronModel, solver: Euler, t0: float, t1: float, dt0: float, y0: PyTree, save_every_n_steps: int = 1, args: PyTree = None):
     """
     Run a simulation using the specified solver and terms.
 
     Steps through the differential equation defined by `terms` from time `t0` to `t1` with increments of `dt0`.
+    y_0 is ((V, conductances, spikes), noise_E, noise_I).
 
     Args:
-        terms (AbstractTerm): The terms defining the differential equation.
+        model (NoisyNeuronModel): The neuron model containing the terms.
         solver (Euler): The solver to use for integration.
         t0 (float): Initial time.
         t1 (float): Final time.
@@ -33,11 +35,18 @@ def run_SNN_simulation(terms: AbstractTerm, solver: Euler, t0: float, t1: float,
     Returns:
         Solution object containing times and states.
     """
+    # Set up solution parameters
     times = jnp.arange(t0, t1, dt0)
     n_saves = len(times) // save_every_n_steps + 1
     ys = jax.tree.map(lambda x: jnp.empty(shape=(n_saves, *x.shape)), y0)
     ts = [t0]
+    terms = model.terms(jrandom.PRNGKey(0))
 
+    # Spiking parameters from neuron model
+    V_threshold = model.network.firing_threshold
+    V_reset = model.network.V_reset
+
+    # Helper function to add current state to ys
     def add_to_ys(ys, y, index):
         return index+1, jax.tree.map(lambda arr, v: arr.at[index].set(v), ys, y)
     
@@ -50,16 +59,17 @@ def run_SNN_simulation(terms: AbstractTerm, solver: Euler, t0: float, t1: float,
             raise RuntimeError(f"Solver step failed with result: {result}")
         step += 1
         
+        V = y[0][0] # y = ((V, conductances, spikes), noise_E, noise_I)
+        spikes = jnp.float32(V > V_threshold)
+        V_new = (1.0 - spikes) * V + spikes * V_reset
+        y = ((V_new, y[0][1], spikes), y[1], y[2]) # reset V and set spikes
+        
         # Save results if at the correct interval
         if step % save_every_n_steps == 0:
             save_index, ys = add_to_ys(ys, y, save_index)
             ts.append(t + dt0)
 
     return Solution(t0=t0, t1=t1, ts=ts, ys=ys, interpolation=None, stats=None, result=RESULTS.successful, solver_state=None, controller_state=None, made_jump=None, event_mask=None)
-            
-
-
-
     
 class SpikingEuler(Euler):
     """
