8635a5bec63b3d8aac2052397cfd7717f2472225
Alexander de Ranitz <alexanderderanitz@gmail.com>
2025-09-29 16:08:36 +0200

    Small fixes

--- src/adaptive_SNN/models/environment.py
+++ src/adaptive_SNN/models/environment.py
@@ -1,7 +1,13 @@
 import diffrax as dfx
 import equinox as eqx
+import jax
 import jax.numpy as jnp
 from jaxtyping import Array
+
+default_float = jnp.float64 if jax.config.jax_enable_x64 else jnp.float32
+
+
+# TODO: Make evironment ABC and have different environment models inherit from it
 
 
 class EnvironmentModel(eqx.Module):
@@ -23,17 +29,17 @@ class EnvironmentModel(eqx.Module):
 
     @property
     def noise_shape(self):
-        return None  # No noise
+        return jax.ShapeDtypeStruct(shape=(self.dim,), dtype=default_float)
 
     def drift(self, t, x, args):
         if args is None or "env_input" not in args:
             raise ValueError(
                 "EnvironmentModel requires 'env_input' in args for drift computation."
             )
-        return args["env_input"]
+        return args["env_input"](t, x, args) - x  # Simple dynamics towards input
 
     def diffusion(self, t, x, args):
-        return None
+        return jnp.zeros((self.dim, self.dim))
 
     def terms(self, key):
         process_noise = dfx.UnsafeBrownianPath(
