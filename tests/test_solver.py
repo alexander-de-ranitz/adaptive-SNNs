29752fcba89aaafd9144967b6cc0e5cea390eb3a
Alexander de Ranitz <alexanderderanitz@gmail.com>
2025-10-14 10:52:27 +0200

    Remove unneeded test

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -163,70 +163,7 @@ def test_solver_timesteps():
     assert jnp.allclose(sol_1_ts, sol_2_ts)
 
 
-def test_solver_output_noiseless():
-    N_neurons = 4
-    N_inputs = 0
-    t0, t1, dt0 = 0.0, 1.0, 0.1
-
-    key = jr.PRNGKey(0)
-    model = _make_noiseless_network(N_neurons, N_inputs, dt0, key)
-
-    # Prepare initial state from model
-    y0 = model.initial
-    solver = dfx.Euler()
-    args = _default_args(N_neurons, N_inputs)
-
-    # Define a save function that extracts only the relevant parts of the state. Necessary because
-    # diffrax does not update the spike buffer in the same way as our custom solver.
-    def save_fn_custom(y: NoisyNetworkState):
-        return (
-            (y.network_state.V, y.network_state.G, y.network_state.S),
-            y.noise_E_state,
-            y.noise_I_state,
-        )
-
-    def save_fn_dfx(t, y: NoisyNetworkState, args):
-        return (
-            (y.network_state.V, y.network_state.G, y.network_state.S),
-            y.noise_E_state,
-            y.noise_I_state,
-        )
-
-    # Our method
-    save_every = 1
-    sol_1 = simulate_noisy_SNN(
-        model,
-        solver,
-        t0,
-        t1,
-        dt0,
-        y0,
-        save_every_n_steps=save_every,
-        save_fn=save_fn_custom,
-        args=args,
-    )
-
-    # Direct diffrax call for comparison
-    terms = model.terms(jr.PRNGKey(0))
-    saveat = dfx.SaveAt(t0=True, t1=True, steps=True, fn=save_fn_dfx)
-    sol_2 = dfx.diffeqsolve(
-        terms,
-        solver,
-        t0=t0,
-        t1=t1,
-        dt0=dt0,
-        y0=y0,
-        saveat=saveat,
-        args=args,
-        adjoint=dfx.ForwardMode(),
-    )
-    sol_2_ts, sol_2_ys = get_non_inf_ts_ys(sol_2)
-
-    assert jnp.allclose(sol_1.ts, sol_2_ts)
-    assert allclose_pytree(sol_1.ys, sol_2_ys)
-
-
-def test_solver_output_with_noise():
+def test_solver_output():
     """Tests that our custom solver function produces the same output as a direct diffrax call.
     Note that this only works when our model does not spike, this is not implemented in the diffrax solver."""
 
