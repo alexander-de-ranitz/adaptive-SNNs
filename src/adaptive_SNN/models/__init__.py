75a7cddc804fd4e5623701c78ee6f4493d807047
Alexander de Ranitz <alexanderderanitz@gmail.com>
2025-10-21 14:49:05 +0200

    Separate agent/reward from env model, add named state classes

--- src/adaptive_SNN/models/__init__.py
+++ src/adaptive_SNN/models/__init__.py
@@ -1,11 +1,12 @@
-from adaptive_SNN.models.agent_system import AgentSystem
+from adaptive_SNN.models.agent_env_system import AgentEnvSystem, SystemState
 from adaptive_SNN.models.base import NeuronModelABC
-from adaptive_SNN.models.environment import EnvironmentModel
+from adaptive_SNN.models.environment import InputTrackingEnvironment
 from adaptive_SNN.models.metrics import (
     compute_charge_ratio,
     compute_conductance_ratio,
     compute_CV_ISI,
 )
+from adaptive_SNN.models.networks.agent import Agent, AgentState
 from adaptive_SNN.models.networks.lif import LIFNetwork, LIFState
 from adaptive_SNN.models.networks.noisy_network import NoisyNetwork, NoisyNetworkState
 from adaptive_SNN.models.noise.oup import OUP
@@ -18,8 +19,11 @@ __all__ = [
     "NoisyNetwork",
     "NoisyNetworkState",
     "OUP",
-    "AgentSystem",
-    "EnvironmentModel",
+    "Agent",
+    "AgentState",
+    "InputTrackingEnvironment",
+    "AgentEnvSystem",
+    "SystemState",
     "RewardModel",
     "compute_conductance_ratio",
     "compute_charge_ratio",
--- src/adaptive_SNN/models/networks/__init__.py
+++ src/adaptive_SNN/models/networks/__init__.py
@@ -1,4 +1,12 @@
+from adaptive_SNN.models.networks.agent import Agent, AgentState
 from adaptive_SNN.models.networks.lif import LIFNetwork, LIFState
 from adaptive_SNN.models.networks.noisy_network import NoisyNetwork, NoisyNetworkState
 
-__all__ = ["LIFNetwork", "LIFState", "NoisyNetwork", "NoisyNetworkState"]
+__all__ = [
+    "LIFNetwork",
+    "LIFState",
+    "NoisyNetwork",
+    "NoisyNetworkState",
+    "Agent",
+    "AgentState",
+]
