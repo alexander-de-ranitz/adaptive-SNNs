f37d6ec7071791a4df56290f1f71a848b75334f6
Alexander de Ranitz <alexanderderanitz@gmail.com>
2025-11-03 16:45:33 +0100

    Improve freq plotting

--- src/adaptive_SNN/visualization/__init__.py
+++ src/adaptive_SNN/visualization/__init__.py
@@ -1,4 +1,4 @@
-from adaptive_SNN.visualization.frequency_analysis import plot_voltage_spectrum
+from adaptive_SNN.visualization.frequency_analysis import plot_frequency_analysis
 from adaptive_SNN.visualization.plot_simulations import (
     plot_learning_results,
     plot_simulate_SNN_results,
@@ -7,5 +7,5 @@ from adaptive_SNN.visualization.plot_simulations import (
 __all__ = [
     "plot_simulate_SNN_results",
     "plot_learning_results",
-    "plot_voltage_spectrum",
+    "plot_frequency_analysis",
 ]
--- src/adaptive_SNN/visualization/frequency_analysis.py
+++ src/adaptive_SNN/visualization/frequency_analysis.py
@@ -3,32 +3,88 @@ import matplotlib.pyplot as plt
 from scipy.signal import welch
 
 from adaptive_SNN.visualization.utils import (
+    _plot_membrane_potential,
+    _plot_voltage_distribution,
+    get_LIF_model,
     get_LIF_state,
-    plot_membrane_potential,
-    plot_voltage_distribution,
+    get_noisy_network_model,
+    get_noisy_network_state,
 )
 
 
-def _plot_frequency_spectrum(ax, t, state, model, neurons_to_plot=None):
-    V = get_LIF_state(state).V
+def _plot_frequency_spectrum(ax, t, signals, labels):
     N_timepoints = t.shape[0]
     dt = t[1] - t[0]
 
-    if neurons_to_plot is None:
-        neurons_to_plot = jnp.arange(V.shape[1])
+    for i, signal in enumerate(signals):
+        f, Pxx = welch(signal, fs=1 / dt, nperseg=min(1 / dt, N_timepoints))
+        f = f[1:]  # Remove DC component
+        Pxx = Pxx[1:]
+        mask = f < 150  # Limit to 150 Hz for better visualization, remove DC component
+        ax.plot(f[mask], Pxx[mask] / jnp.sum(Pxx[mask]), label=labels[i])
 
-    for neuron_idx in neurons_to_plot:
-        f, Pxx = welch(V[:, neuron_idx], fs=1 / dt, nperseg=min(1 / dt, N_timepoints))
-        mask = f < 150  # Limit to 150 Hz for better visualization
-        ax.plot(f[mask], Pxx[mask], label=f"Neuron {neuron_idx}")
-
-    ax.set_xlabel("Frequency (Hz)")
-    ax.set_ylabel("Power (log scale)")
+    ax.set_xlabel("Log Frequency (Hz)")
+    ax.set_ylabel("Log Power Density")
     ax.set_yscale("log")
+    ax.set_xscale("log")
     ax.legend()
 
 
-def plot_voltage_spectrum(
+def _plot_voltage_frequency_spectrum(ax, t, state, model, neurons_to_plot=None):
+    V = get_LIF_state(state).V
+    if neurons_to_plot is None:
+        neurons_to_plot = jnp.arange(V.shape[1])
+    signals = [V[:, i] for i in neurons_to_plot]
+    labels = [f"Neuron {i + 1}" for i in neurons_to_plot]
+    _plot_frequency_spectrum(ax, t, signals, labels)
+
+
+def _plot_conductance_frequency_spectrum(
+    ax, t, state, model, neurons_to_plot=None, plot_noise=False
+):
+    lif_state = get_LIF_state(state)
+    G = lif_state.G
+    W = jnp.where(~jnp.isfinite(lif_state.W), 0.0, lif_state.W)
+
+    exc_mask = get_LIF_model(model).excitatory_mask
+    weighed_G_excitatory = jnp.sum(W * G * exc_mask[None, :], axis=-1)
+
+    if neurons_to_plot is None:
+        neurons_to_plot = jnp.arange(weighed_G_excitatory.shape[1])
+
+    signals = [weighed_G_excitatory[:, i] for i in neurons_to_plot]
+    labels = [f"Neuron {i + 1} Syn E Conductance" for i in neurons_to_plot]
+    _plot_frequency_spectrum(ax, t, signals, labels)
+
+    # Plot the empirical and expected spectrum of the noise
+    if plot_noise:
+        noisy_network_state = get_noisy_network_state(state)
+        noise_E = noisy_network_state.noise_E_state
+        noise_signals = [noise_E[:, i] for i in neurons_to_plot]
+        noise_labels = [f"Neuron {i + 1} Noise E Conductance" for i in neurons_to_plot]
+        _plot_frequency_spectrum(ax, t, noise_signals, noise_labels)
+
+        # Plot expected PSD for OU process
+        noise_model = get_noisy_network_model(model).noise_E
+        expected_PSD = (
+            lambda f: 2
+            * noise_model.noise_scale
+            * noise_model.tau**2
+            / (1 + (2 * jnp.pi * f * noise_model.tau) ** 2)
+        )
+        f_vals = jnp.linspace(0, 150, 150)
+        ax.plot(
+            f_vals,
+            expected_PSD(f_vals) / jnp.sum(expected_PSD(f_vals)),
+            label="Expected OU PSD",
+            linestyle="--",
+            color="k",
+        )
+    ax.set_title("Excitatory Conductance PSD")
+    ax.legend()
+
+
+def plot_frequency_analysis(
     sol,
     model,
     t0: float,
@@ -43,19 +99,21 @@ def plot_voltage_spectrum(
     if neurons_to_plot is None:
         neurons_to_plot = jnp.arange(V.shape[1])
 
-    fig, axs = plt.subplots(3, 1, figsize=(8, 5))
+    fig, axs = plt.subplots(3, 1, figsize=(8, 8))
 
     # Plot membrane potential
-    plot_membrane_potential(axs[0], t, state, model, neurons_to_plot=neurons_to_plot)
+    _plot_membrane_potential(axs[0], t, state, model, neurons_to_plot=neurons_to_plot)
     axs[0].set_title("Neuron Membrane Potential")
 
     # Plot voltage distribution
-    plot_voltage_distribution(axs[1], t, state, model, neurons_to_plot=neurons_to_plot)
+    _plot_voltage_distribution(axs[1], t, state, model, neurons_to_plot=neurons_to_plot)
     axs[1].set_title("Voltage Distribution")
 
     # Plot freq spectrum
-    _plot_frequency_spectrum(axs[2], t, state, model, neurons_to_plot=neurons_to_plot)
-    axs[2].set_title("Voltage Frequency Spectrum")
+    _plot_conductance_frequency_spectrum(
+        axs[2], t, state, model, neurons_to_plot=neurons_to_plot, plot_noise=True
+    )
+    axs[2].set_title("Conductance Frequency Spectrum")
 
     plt.tight_layout()
     plt.show()
