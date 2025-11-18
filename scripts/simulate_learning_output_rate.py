f61ac9279b2077486b61d6a3a960d23bf93b7146
Alexander de Ranitz <alexanderderanitz@gmail.com>
2025-11-18 09:46:36 +0100

    Update scripts

--- scripts/simulate_learning_output_rate.py
+++ scripts/simulate_learning_output_rate.py
@@ -16,38 +16,69 @@ from adaptive_SNN.models import (
 from adaptive_SNN.models.environment import SpikeRateEnvironment
 from adaptive_SNN.models.reward import RewardModel
 from adaptive_SNN.solver import simulate_noisy_SNN
+from adaptive_SNN.utils.analytics import (
+    compute_expected_synaptic_std,
+    compute_oup_diffusion_coefficient,
+    compute_required_input_weight,
+)
 from adaptive_SNN.visualization import plot_learning_results
 
 
 def main():
     t0 = 0
     t1 = 100
-    dt0 = 1e-4
+    dt = 1e-4
     key = jr.PRNGKey(0)
     key, _ = jr.split(key)
     N_neurons = 1
     N_inputs = 2
 
+    # Define input parameters
+    exc_rate = 5000
+    exc_to_inh_ratio = 4.0
+    inh_rate = exc_rate / exc_to_inh_ratio
+    rates = jnp.array([exc_rate, inh_rate])  # firing rate in Hz
+
+    target_state = 10.0  # Target output state
+
+    input_weight = compute_required_input_weight(
+        target_mean_g_syn=10e-9,
+        N_inputs=1.0,
+        tau=LIFNetwork.tau_E,
+        input_rate=exc_rate,
+        synaptic_increment=LIFNetwork.synaptic_increment,
+    )
+
     # Set up models
     neuron_model = LIFNetwork(
         N_neurons=N_neurons,
         N_inputs=N_inputs,
-        dt=dt0,
-        input_neuron_types=jnp.array([1.0, 0.0]),
+        dt=dt,
         fully_connected_input=True,
-        input_weight=1.0,
+        input_weight=input_weight,
+        fraction_excitatory_input=0.5,
         key=key,
     )
     key, _ = jr.split(key)
 
-    noise_E_model = OUP(tau=250.0, noise_scale=1e-7, mean=0.0, dim=N_neurons)
-    noise_I_model = OUP(tau=250.0, noise_scale=1e-7, mean=0.0, dim=N_neurons)
+    syn_std = compute_expected_synaptic_std(
+        N_inputs=1.0,
+        input_rate=exc_rate,
+        tau=neuron_model.tau_E,
+        synaptic_increment=neuron_model.synaptic_increment,
+        input_weight=input_weight,
+    )
+
+    D = compute_oup_diffusion_coefficient(
+        target_std=syn_std * 2, tau=neuron_model.tau_E
+    )
+    noise_model = OUP(tau=neuron_model.tau_E, noise_scale=D, dim=N_neurons)
 
     network = NoisyNetwork(
         neuron_model=neuron_model,
-        noise_E_model=noise_E_model,
-        noise_I_model=noise_I_model,
+        noise_model=noise_model,
     )
+
     agent = Agent(
         neuron_model=network,
         reward_model=RewardModel(reward_rate=10),
@@ -62,18 +93,10 @@ def main():
     solver = dfx.EulerHeun()
     init_state = model.initial
 
-    # Define input parameters
-    exc_rate = 5000
-    exc_to_inh_ratio = 4.0
-    inh_rate = exc_rate / exc_to_inh_ratio
-    rate = jnp.array([exc_rate, inh_rate])  # firing rate in Hz
-
-    target_state = 10.0  # Target output state
-
     # Define args
     # Create a base seed that's unique for each loop iteration
     args = {
-        "get_learning_rate": lambda t, x, args: jnp.where(t < 10, 0.0, 0.01),
+        "get_learning_rate": lambda t, x, args: jnp.where(t < 1, 0.0, 0.025),
         "network_output_fn": lambda t, agent_state, args: jnp.squeeze(
             agent_state.noisy_network.network_state.S[0]
         ),
@@ -81,7 +104,9 @@ def main():
             environment_state[0] - target_state
         ),
         "get_input_spikes": lambda t, x, args: jr.poisson(
-            jr.PRNGKey((t / dt0).astype(int)), rate * dt0, shape=(N_inputs,)
+            jr.PRNGKey(jnp.int32(jnp.round(t / dt))),
+            rates * dt,
+            shape=(N_neurons, N_inputs),
         ),
         "get_desired_balance": lambda t, x, args: jnp.array([4.0]),
     }
@@ -92,7 +117,7 @@ def main():
         return state.agent_state.noisy_network.network_state.W
 
     sols = []
-    for i in range(3):
+    for i in range(len(initial_weight_factors)):
         init = eqx.tree_at(
             get_weights,
             init_state,
@@ -101,34 +126,41 @@ def main():
         key = jr.PRNGKey(i * 12345)
         print(f"Running simulation {i + 1}/3 ...")
         start = time.time()
+
+        def save_fn(y: SystemState):
+            return eqx.tree_at(
+                lambda s: (
+                    s.agent_state.noisy_network.network_state.spike_buffer,
+                    s.agent_state.noisy_network.network_state.G,
+                ),
+                y,
+                replace_fn=lambda x: None,
+            )
+
         sol = simulate_noisy_SNN(
             model,
             solver,
             t0,
             t1,
-            dt0,
+            dt,
             init,
             save_every_n_steps=1000,
             args=args,
+            save_fn=save_fn,
             key=key,
         )
         end = time.time()
         print(f"Simulation completed in {end - start:.2f} seconds.")
 
         sols.append(sol)
-        # print("Plotting results...")
-        # plot_learning_results(sol, model, t0, t1, dt0, args, save_path=f"../figures/learning_output_rate_{i}" + ".png")
 
-    print("Plotting all results together...")
+    print("Plotting results...")
     plot_learning_results(
         sols,
         model,
-        t0,
-        t1,
-        dt0,
         args,
         target_state,
-        save_path="../figures/learning_output_rate_batched.png",
+        save_path="../figures/learning_output_rate_batched_new.png",
     )
 
 
--- scripts/simulate_network.py
+++ scripts/simulate_network.py
@@ -4,101 +4,121 @@ from jax import numpy as jnp
 
 from adaptive_SNN.models import (
     OUP,
-    Agent,
-    AgentEnvSystem,
     LIFNetwork,
+    LIFState,
     NoisyNetwork,
+    NoisyNetworkState,
 )
-from adaptive_SNN.models.environment import SpikeRateEnvironment
-from adaptive_SNN.models.reward import RewardModel
 from adaptive_SNN.solver import simulate_noisy_SNN
-from adaptive_SNN.visualization import (
-    plot_frequency_analysis,
-    plot_simulate_SNN_results,
+from adaptive_SNN.utils.analytics import (
+    compute_expected_synaptic_std,
+    compute_oup_diffusion_coefficient,
+    compute_required_input_weight,
 )
+from adaptive_SNN.visualization import plot_network_stats
 
 
 def main():
     t0 = 0
-    t1 = 10
+    t1 = 1.0
     dt0 = 1e-4
     key = jr.PRNGKey(1)
-    N_neurons = 1
-    N_inputs = 2
+
+    N_neurons = 100
+    N_inputs = 100
 
     # Define input parameters
-    exc_rate = 5000
-    exc_to_inh_ratio = 4.0
-    inh_rate = exc_rate / exc_to_inh_ratio
-    rate = jnp.array([exc_rate, inh_rate])  # firing rate in Hz
+    target_firing_rate = 20
+    input_firing_rate = 20
+
+    computed_input_weight = compute_required_input_weight(
+        target_mean_g_syn=50e-9,
+        N_inputs=jnp.ceil((N_inputs + N_neurons) * 0.8 * 0.1),
+        tau=LIFNetwork.tau_E,
+        input_rate=target_firing_rate,
+        synaptic_increment=LIFNetwork.synaptic_increment,
+    )
+
+    # The computed input weight is way too large! This needs to be tuned
+    rec_weight = computed_input_weight * 0.15
+    input_weight = rec_weight
+
+    balance = 5.0
 
     # Set up models
     neuron_model = LIFNetwork(
         N_neurons=N_neurons,
         N_inputs=N_inputs,
         dt=dt0,
-        input_neuron_types=jnp.array([1.0, 0.0]),
-        fully_connected_input=True,
-        input_weight=2.0,
+        fully_connected_input=False,
+        input_weight=input_weight,
+        rec_weight=rec_weight,
         key=key,
     )
 
-    expected_syn_std = jnp.sqrt(
-        0.5
-        * exc_rate
-        * neuron_model.tau_E
-        * (neuron_model.synaptic_increment**2)
-        * (neuron_model.input_weight**2)
+    expected_syn_std = compute_expected_synaptic_std(
+        N_inputs=0.1 * (N_inputs + N_neurons),
+        input_rate=target_firing_rate,
+        tau=neuron_model.tau_E,
+        synaptic_increment=neuron_model.synaptic_increment,
+        input_weight=neuron_model.input_weight,
     )
-    OUP_std = 0.1 * expected_syn_std
-    D = 2 * (OUP_std**2) / neuron_model.tau_E
+    D = compute_oup_diffusion_coefficient(
+        target_std=0.2 * expected_syn_std,
+        tau=neuron_model.tau_E,
+    )
 
-    noise_model = OUP(tau=neuron_model.tau_E, noise_scale=D, mean=0.0, dim=N_neurons)
+    noise_model = OUP(
+        tau=neuron_model.tau_E, noise_scale=0 * D, mean=0.0, dim=N_neurons
+    )
 
-    network = NoisyNetwork(
+    model = NoisyNetwork(
         neuron_model=neuron_model,
         noise_model=noise_model,
     )
-    agent = Agent(
-        neuron_model=network,
-        reward_model=RewardModel(reward_rate=10),
-    )
 
-    model = AgentEnvSystem(
-        agent=agent,
-        environment=SpikeRateEnvironment(
-            dim=1,
-        ),
-    )
     solver = dfx.EulerHeun()
     init_state = model.initial
 
-    target_state = 10.0  # Target output state
-
     # Define args
     args = {
-        "network_output_fn": lambda t, agent_state, args: jnp.squeeze(
-            agent_state.noisy_network.network_state.S[0]
-        ),
-        "reward_fn": lambda t, environment_state, args: -jnp.abs(
-            environment_state[0] - target_state
-        ),
-        "get_desired_balance": lambda t, x, args: jnp.array([4.0]),
+        "get_desired_balance": lambda t, x, args: jnp.array([balance]),
         "get_input_spikes": lambda t, x, args: jr.poisson(
-            jr.PRNGKey((t / dt0).astype(int)), rate * dt0, shape=(N_inputs,)
+            jr.PRNGKey((t / dt0).astype(int)),
+            input_firing_rate * dt0,
+            shape=(N_inputs,),
         ),
     }
 
+    def save_fn(y: NoisyNetworkState):
+        return LIFState(
+            V=y.network_state.V,
+            S=y.network_state.S,
+            G=y.network_state.G,
+            W=y.network_state.W,
+            time_since_last_spike=None,
+            buffer_index=None,
+            spike_buffer=None,
+        )
+
     print("Running simulation...")
     sol = simulate_noisy_SNN(
-        model, solver, t0, t1, dt0, init_state, save_every_n_steps=1, args=args
+        model,
+        solver,
+        t0,
+        t1,
+        dt0,
+        init_state,
+        save_every_n_steps=1,
+        args=args,
+        save_fn=save_fn,
     )
 
-    print("Plotting results...")
-    plot_simulate_SNN_results(
-        sol, model, t0, t1, dt0, args, neurons_to_plot=jnp.array([0])
+    print("Simulation complete. Generating plots...")
+    plot_network_stats(
+        sol,
+        model,
     )
-    plot_frequency_analysis(sol, model, t0, t1, dt0, neurons_to_plot=jnp.array([0]))
 
 
 if __name__ == "__main__":
