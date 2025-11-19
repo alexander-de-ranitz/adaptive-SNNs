18dcdb8f0360514321c19c803c7f35cac7b7504d
Alexander de Ranitz <alexanderderanitz@gmail.com>
2025-11-19 11:43:45 +0100

    test several lr's and noise levels on rate task

--- scripts/simulate_learning_output_rate.py
+++ scripts/simulate_learning_output_rate.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import diffrax as dfx
@@ -17,10 +18,9 @@ from adaptive_SNN.models.environment import SpikeRateEnvironment
 from adaptive_SNN.models.reward import RewardModel
 from adaptive_SNN.solver import simulate_noisy_SNN
 from adaptive_SNN.utils.analytics import (
-    compute_expected_synaptic_std,
-    compute_oup_diffusion_coefficient,
     compute_required_input_weight,
 )
+from adaptive_SNN.utils.save_helper import save_part_of_state
 from adaptive_SNN.visualization import plot_learning_results
 
 
@@ -41,127 +41,126 @@ def main():
 
     target_state = 10.0  # Target output state
 
-    input_weight = compute_required_input_weight(
-        target_mean_g_syn=10e-9,
-        N_inputs=1.0,
-        tau=LIFNetwork.tau_E,
-        input_rate=exc_rate,
-        synaptic_increment=LIFNetwork.synaptic_increment,
-    )
+    lrs = [0.05, 0.2, 0.5]
+    noise_levels = [0.05, 0.2, 0.5]
+    for lr in lrs:
+        for noise_level in noise_levels:
+            if os.path.exists(
+                f"../figures/learning_output_rate_noise_{noise_level}_lr_{lr}.png"
+            ):
+                print(
+                    f"Figure for noise level {noise_level} and lr {lr} already exists. Skipping simulation."
+                )
+                continue
 
-    # Set up models
-    neuron_model = LIFNetwork(
-        N_neurons=N_neurons,
-        N_inputs=N_inputs,
-        dt=dt,
-        fully_connected_input=True,
-        input_weight=input_weight,
-        fraction_excitatory_input=0.5,
-        key=key,
-    )
-    key, _ = jr.split(key)
-
-    syn_std = compute_expected_synaptic_std(
-        N_inputs=1.0,
-        input_rate=exc_rate,
-        tau=neuron_model.tau_E,
-        synaptic_increment=neuron_model.synaptic_increment,
-        input_weight=input_weight,
-    )
-
-    D = compute_oup_diffusion_coefficient(
-        target_std=syn_std * 2, tau=neuron_model.tau_E
-    )
-    noise_model = OUP(tau=neuron_model.tau_E, noise_scale=D, dim=N_neurons)
-
-    network = NoisyNetwork(
-        neuron_model=neuron_model,
-        noise_model=noise_model,
-    )
-
-    agent = Agent(
-        neuron_model=network,
-        reward_model=RewardModel(reward_rate=10),
-    )
-
-    model = AgentEnvSystem(
-        agent=agent,
-        environment=SpikeRateEnvironment(
-            dim=1,
-        ),
-    )
-    solver = dfx.EulerHeun()
-    init_state = model.initial
-
-    # Define args
-    # Create a base seed that's unique for each loop iteration
-    args = {
-        "get_learning_rate": lambda t, x, args: jnp.where(t < 1, 0.0, 0.025),
-        "network_output_fn": lambda t, agent_state, args: jnp.squeeze(
-            agent_state.noisy_network.network_state.S[0]
-        ),
-        "reward_fn": lambda t, environment_state, args: -jnp.abs(
-            environment_state[0] - target_state
-        ),
-        "get_input_spikes": lambda t, x, args: jr.poisson(
-            jr.PRNGKey(jnp.int32(jnp.round(t / dt))),
-            rates * dt,
-            shape=(N_neurons, N_inputs),
-        ),
-        "get_desired_balance": lambda t, x, args: jnp.array([4.0]),
-    }
-
-    initial_weight_factors = jnp.array([1.5, 2.5, 3.5])
-
-    def get_weights(state: SystemState):
-        return state.agent_state.noisy_network.network_state.W
-
-    sols = []
-    for i in range(len(initial_weight_factors)):
-        init = eqx.tree_at(
-            get_weights,
-            init_state,
-            get_weights(init_state) * initial_weight_factors[i],
-        )
-        key = jr.PRNGKey(i * 12345)
-        print(f"Running simulation {i + 1}/3 ...")
-        start = time.time()
-
-        def save_fn(y: SystemState):
-            return eqx.tree_at(
-                lambda s: (
-                    s.agent_state.noisy_network.network_state.spike_buffer,
-                    s.agent_state.noisy_network.network_state.G,
-                ),
-                y,
-                replace_fn=lambda x: None,
+            print(f"Running simulations with noise level: {noise_level}")
+            input_weight = compute_required_input_weight(
+                target_mean_g_syn=50e-9,
+                N_inputs=1.0,
+                tau=LIFNetwork.tau_E,
+                input_rate=exc_rate,
+                synaptic_increment=LIFNetwork.synaptic_increment,
             )
 
-        sol = simulate_noisy_SNN(
-            model,
-            solver,
-            t0,
-            t1,
-            dt,
-            init,
-            save_every_n_steps=1000,
-            args=args,
-            save_fn=save_fn,
-            key=key,
-        )
-        end = time.time()
-        print(f"Simulation completed in {end - start:.2f} seconds.")
+            # Set up models
+            neuron_model = LIFNetwork(
+                N_neurons=N_neurons,
+                N_inputs=N_inputs,
+                dt=dt,
+                fully_connected_input=True,
+                input_weight=input_weight,
+                fraction_excitatory_input=0.5,
+                key=key,
+            )
+            key, _ = jr.split(key)
 
-        sols.append(sol)
+            noise_model = OUP(tau=neuron_model.tau_E, dim=N_neurons)
 
-    print("Plotting results...")
-    plot_learning_results(
-        sols,
-        model,
-        args,
-        target_state,
-        save_path="../figures/learning_output_rate_batched_new.png",
-    )
+            network = NoisyNetwork(
+                neuron_model=neuron_model,
+                noise_model=noise_model,
+            )
+
+            agent = Agent(
+                neuron_model=network,
+                reward_model=RewardModel(reward_rate=10),
+            )
+
+            model = AgentEnvSystem(
+                agent=agent,
+                environment=SpikeRateEnvironment(
+                    dim=1,
+                ),
+            )
+            solver = dfx.EulerHeun()
+            init_state = model.initial
+
+            # Define args
+            # Create a base seed that's unique for each loop iteration
+            args = {
+                "get_learning_rate": lambda t, x, args: jnp.where(t < 5, 0.0, lr),
+                "network_output_fn": lambda t, agent_state, args: jnp.squeeze(
+                    agent_state.noisy_network.network_state.S[0]
+                ),
+                "reward_fn": lambda t, environment_state, args: -jnp.abs(
+                    environment_state[0] - target_state
+                ),
+                "get_input_spikes": lambda t, x, args: jr.poisson(
+                    jr.PRNGKey(jnp.int32(jnp.round(t / dt))),
+                    rates * dt,
+                    shape=(N_neurons, N_inputs),
+                ),
+                "get_desired_balance": lambda t, x, args: jnp.array([4.0]),
+                "noise_scale_hyperparam": noise_level,
+            }
+
+            initial_weight_factors = jnp.array([0.5, 1.0, 2.0])
+
+            def get_weights(state: SystemState):
+                return state.agent_state.noisy_network.network_state.W
+
+            sols = []
+            for i in range(len(initial_weight_factors)):
+                init = eqx.tree_at(
+                    get_weights,
+                    init_state,
+                    get_weights(init_state) * initial_weight_factors[i],
+                )
+                key = jr.PRNGKey(i * 12345)
+                print(f"Running simulation {i + 1}/{len(initial_weight_factors)} ...")
+                start = time.time()
+
+                def save_fn(y: SystemState):
+                    return save_part_of_state(
+                        y, reward=True, W=True, environment_state=True
+                    )
+
+                sol = simulate_noisy_SNN(
+                    model,
+                    solver,
+                    t0,
+                    t1,
+                    dt,
+                    init,
+                    save_every_n_steps=1000,
+                    args=args,
+                    save_fn=save_fn,
+                    key=key,
+                )
+                end = time.time()
+                print(f"Simulation completed in {end - start:.2f} seconds.")
+
+                sols.append(sol)
+
+            print("Plotting results...")
+
+            plot_learning_results(
+                sols,
+                model,
+                args,
+                target_state,
+                save_path=f"../figures/learning_output_rate_noise_{noise_level}_lr_{lr}.png",
+            )
 
 
 if __name__ == "__main__":
