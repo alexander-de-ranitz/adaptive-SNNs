5689c50b63af8c9271a662bf748374e6237175e1
Alexander de Ranitz <alexanderderanitz@gmail.com>
2025-12-04 16:57:57 +0100

    Add plotting function for double integrator

--- src/adaptive_SNN/visualization/api/plotting.py
+++ src/adaptive_SNN/visualization/api/plotting.py
@@ -161,6 +161,84 @@ def plot_learning_results(
         plt.show()
 
 
+def plot_SDI_results(
+    sol: Solution,
+    model: AgentEnvSystem,
+    args: dict,
+    save_path: str | None = None,
+):
+    if isinstance(sol, Solution):
+        sol = [sol]
+
+    fig, axs = plt.subplots(4, 1, figsize=(10, 8), sharex=True)
+
+    colors = ["k", "b", "g", "m", "c", "r", "y", "k"]
+
+    for i, sol in enumerate(sol):
+        # Get results
+        t = sol.ts
+        t0 = t[0]
+        t1 = t[-1]
+
+        state: SystemState = sol.ys
+        agent_state, env_state = state.agent_state, state.environment_state
+        reward_state = agent_state.reward
+
+        # Compute reward prediction error if possible
+        if args is not None and "reward_fn" in args:
+            rewards = jnp.array(
+                [
+                    args["reward_fn"](
+                        ti, jax.tree.map(lambda arr: arr[i], env_state), args
+                    )
+                    for i, ti in enumerate(t)
+                ]
+            )
+            RPE = jnp.squeeze(rewards) - jnp.squeeze(reward_state)
+
+        else:
+            RPE = None
+
+        for ax in axs:
+            ax.set_xlim(t0, t1)
+
+        axs[0].plot(t, env_state, label=["Position", "Velocity"])
+        axs[0].legend()
+        axs[0].set_title("Environment State ")
+        axs[0].set_ylabel("Environment State")
+
+        axs[1].plot(t, reward_state, label="Reward State", color=colors[i])
+        axs[1].plot(t, rewards, label="Instant Rewards", color="k", linestyle="--")
+        axs[1].legend(loc="upper right")
+        axs[1].set_title("Rewards Over Time")
+        axs[1].set_ylabel("Reward")
+
+        axs[2].plot(t, RPE, label="Reward Prediction Error", color=colors[i])
+        axs[2].set_title("Reward Prediction Error")
+        axs[2].set_ylabel("RPE")
+
+        if args is not None and "network_output_fn" in args:
+            outputs = jnp.array(
+                [
+                    args["network_output_fn"](
+                        ti, jax.tree.map(lambda arr: arr[i], agent_state), args
+                    )
+                    for i, ti in enumerate(t)
+                ]
+            )
+            axs[3].plot(t, outputs, label="Network Output", color=colors[i])
+            axs[3].set_title("Network Output Over Time")
+            axs[3].set_ylabel("Network Output")
+            axs[3].set_xlabel("Time (s)")
+    plt.tight_layout()
+
+    if save_path is not None:
+        plt.savefig(save_path)
+        plt.close()
+    else:
+        plt.show()
+
+
 def plot_network_stats(
     sol: Solution,
     model,
