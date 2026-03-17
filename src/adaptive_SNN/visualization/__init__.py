1ac712092cbb11431df7e7723a1ee91993b1fd2f
Alexander de Ranitz <alexanderderanitz@gmail.com>
2026-03-17 10:04:12 +0100

    Update init

--- src/adaptive_SNN/visualization/__init__.py
+++ src/adaptive_SNN/visualization/__init__.py
@@ -1,16 +1,16 @@
 from adaptive_SNN.visualization.api.plotting import (
     plot_frequency_analysis,
     plot_learning_detailed,
-    plot_learning_results,
     plot_network_stats,
     plot_noise_STA,
+    plot_rate_learning_results,
     plot_SDI_results,
     plot_simulate_SNN_results,
 )
 
 __all__ = [
     "plot_simulate_SNN_results",
-    "plot_learning_results",
+    "plot_rate_learning_results",
     "plot_frequency_analysis",
     "plot_network_stats",
     "plot_noise_STA",
