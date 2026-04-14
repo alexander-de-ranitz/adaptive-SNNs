aa368576f9b9e4acf929c2f041774253723ff552
Alexander de Ranitz <alexanderderanitz@gmail.com>
2026-04-14 15:11:10 +0200

    Add noise models to __init__

--- src/adaptive_SNN/models/noise/__init__.py
+++ src/adaptive_SNN/models/noise/__init__.py
@@ -1,4 +1,5 @@
 # src/adaptive_SNN/models/noise/__init__.py
-from adaptive_SNN.models.noise.oup import OUP
+from adaptive_SNN.models.noise.oup import OUP, NeuralNoiseOUP
+from adaptive_SNN.models.noise.poisson_jump import PoissonJumpProcess
 
-__all__ = ["OUP"]
+__all__ = ["NeuralNoiseOUP", "OUP", "PoissonJumpProcess"]
