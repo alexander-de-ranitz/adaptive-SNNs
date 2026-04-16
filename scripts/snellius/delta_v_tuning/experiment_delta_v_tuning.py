bec9aa3176480ee0729ee4a8ef105774db4bd3ad
Alexander de Ranitz <alexanderderanitz@gmail.com>
2026-04-16 11:34:54 +0200

    Add scripts to run delta v tuning experiment

---
+++ scripts/snellius/delta_v_tuning/experiment_delta_v_tuning.py
@@ -0,0 +1,75 @@
+import jax
+
+jax.config.update(
+    "jax_enable_x64", True
+)  # Enable 64-bit precision for better numerical stability
+
+import argparse
+import time
+
+import jax.numpy as jnp
+import jax.random as jr
+from diffrax import SaveAt
+
+from adaptive_SNN.models.networks.coupled import (
+    CoupledNoiseEligibilityLIFNetwork,
+    CoupledNoiseGatedLIFNetwork,
+)
+from adaptive_SNN.simulation_configs.single_synapse_learning import (
+    create_default_config_single_synapse_task,
+)
+from adaptive_SNN.utils.runner import run_simulation
+from adaptive_SNN.utils.save_helper import save_part_of_state
+
+
+def main():
+    start = time.time()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--delta_V", type=float, default=0.001, help="Steepness of the gating function"
+    )
+
+    parser.add_argument(
+        "--output_file", type=str, default=None, help="Output file name"
+    )
+    parser.add_argument("--key_seed", type=int, default=0, help="Random key seed")
+
+    args = parser.parse_args()
+
+    cfg = create_default_config_single_synapse_task(
+        RPE_noise_rate=1.0,
+        key=jr.PRNGKey(args.key_seed),
+    )
+
+    cfg.save_at = SaveAt(
+        ts=jnp.linspace(cfg.t0, cfg.t1, 50000),
+        fn=lambda t, x, args: save_part_of_state(
+            x,
+            RPE=True,
+            reward_noise=True,
+            eligibility=True,
+        ),
+    )
+
+    cfg.t1 = 300
+    cfg.args.update({"delta_V": args.delta_V})
+    if args.delta_V == 0.0:
+        print("NO GATING")
+        model_cls = CoupledNoiseEligibilityLIFNetwork
+    else:
+        model_cls = CoupledNoiseGatedLIFNetwork
+
+    file_suffix = "_gated" if model_cls == CoupledNoiseGatedLIFNetwork else "_default"
+    cfg.save_file = (
+        args.output_file if args.output_file is not None else ""
+    ) + file_suffix
+    print(f"Running simulation with model class {model_cls.__name__}...")
+    cfg.base_network_cls = model_cls
+    sol, model = run_simulation(cfg, save_results=True)
+    end = time.time()
+    print(f"Simulation completed in {end - start:.2f} seconds")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/snellius/delta_v_tuning/launch.py
@@ -0,0 +1,91 @@
+import argparse
+import shlex
+import sys
+from pathlib import Path
+
+import jax
+
+jax.config.update("jax_enable_x64", True)
+from qcg.pilotjob.api.job import Jobs
+from qcg.pilotjob.api.manager import LocalManager
+
+
+def create_jobs():
+    base_dir = Path(__file__).resolve().parent.parent.parent.parent
+    module_path = "scripts.snellius.delta_v_tuning.experiment_delta_v_tuning"
+
+    parser = argparse.ArgumentParser(description="Launch jobs on Snellius")
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="results/default_folder/",
+        help="Directory to store output files",
+    )
+    cmd_args = parser.parse_args()
+
+    log_dir = (
+        Path(cmd_args.output_dir) / "logs" if cmd_args.output_dir else base_dir / "logs"
+    )
+    results_dir = (
+        Path(cmd_args.output_dir) / "results"
+        if cmd_args.output_dir
+        else base_dir / "results"
+    )
+
+    log_dir.mkdir(parents=True, exist_ok=True)
+    results_dir.mkdir(parents=True, exist_ok=True)
+
+    jobs = Jobs()
+
+    seed = 1241
+    num_iterations = 30
+    for i in range(num_iterations):
+        seed += 129
+        for delta_v in [0.03, 0.01, 0.005, 0.0025, 0.001, 0.0005, 0.0]:
+            id = f"dv_{delta_v:6f}_" if type(delta_v) is float else "no_gating_"
+            name = id + f"seed_{seed}"
+
+            cmd_args = [
+                "--delta_V",
+                str(delta_v),
+                "--key_seed",
+                str(seed),
+                "--output_file",
+                str(results_dir / name),
+            ]
+
+            # Create a bash script to set PYTHONPATH and run the processing script
+            bash_script = "\n".join(
+                [
+                    "#!/usr/bin/env bash",
+                    "set -euo pipefail",
+                    f'export PYTHONPATH={shlex.quote(str(base_dir))}:"${{PYTHONPATH:-}}"',
+                    f"{shlex.quote(sys.executable)} -m {shlex.quote(module_path)} {' '.join(shlex.quote(arg) for arg in cmd_args)}",
+                ]
+            )
+
+            jobs.add(
+                name=name,
+                script=bash_script,
+                stdout=str(log_dir / f"job.out.{name}"),
+                stderr=str(log_dir / f"job.err.{name}"),
+                wd=str(base_dir),
+                numCores=1,
+                iteration=1,
+            )
+
+    return jobs
+
+
+def main():
+    jobs = create_jobs()
+    manager = LocalManager()
+
+    print(f"Submitting {len(jobs.jobs())} jobs to the local manager...")
+    manager.submit(jobs)
+    manager.wait4all()
+    manager.finish()
+
+
+if __name__ == "__main__":
+    main()
