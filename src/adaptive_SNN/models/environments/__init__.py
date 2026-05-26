186a57a4e47169ce3764d0086b461d228f13c5e5
Alexander de Ranitz <alexanderderanitz@gmail.com>
2026-05-26 16:05:32 +0200

    Remove unused SDI environment

--- src/adaptive_SNN/models/environments/__init__.py
+++ src/adaptive_SNN/models/environments/__init__.py
@@ -1,13 +1,7 @@
-from adaptive_SNN.models.environments.double_integrator import (
-    DoubleIntegrator,
-    DoubleIntegratorKickControl,
-)
 from adaptive_SNN.models.environments.input_tracking import InputTrackingEnvironment
 from adaptive_SNN.models.environments.spike_rate import SpikeRateEnvironment
 
 __all__ = [
     "InputTrackingEnvironment",
     "SpikeRateEnvironment",
-    "DoubleIntegrator",
-    "DoubleIntegratorKickControl",
 ]
