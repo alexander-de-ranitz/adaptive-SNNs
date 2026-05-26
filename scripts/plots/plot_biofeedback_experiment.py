65a2919cc087f0a2865c687e16fbd3f26aded6dc
Alexander de Ranitz <alexanderderanitz@gmail.com>
2026-05-26 10:35:48 +0200

    Update plot script

--- scripts/plots/plot_biofeedback_experiment.py
+++ scripts/plots/plot_biofeedback_experiment.py
@@ -4,7 +4,7 @@ import pandas as pd
 from jax import numpy as jnp
 from matplotlib import pyplot as plt
 
-DATA_DIR = Path("results/biofeedback_20260519_155837/results")
+DATA_DIR = Path("results/biofeedback_20260520_173251/results")
 OUTPUT_PATH = Path("figures/biofeedback")
 
 
@@ -12,7 +12,9 @@ def parse_run_file(file: Path):
     parts = file.stem.split("_")
     method = parts[0]
     lr = float(parts[2].replace("seed", ""))
-    assert method in {"gated", "eligibility"}, method
+    if method == "eligibility":
+        method = "default"
+    assert method in {"gated", "default"}, method
     return method, lr
 
 
@@ -31,14 +33,14 @@ def load_all_data(data_dir: Path) -> pd.DataFrame:
         run_files.append([*metadata, *data, ts])
     return pd.DataFrame(
         run_files,
-        columns=["method", "lr", "S", "RPE", "W_target", "W_mean", "env_state", "ts"],
+        columns=["method", "lr", "env_state", "target_w", "mean_w", "std_w", "ts"],
     )
 
 
 def main():
     df = load_all_data(DATA_DIR)
     print(df.head())
-    fig, axs = plt.subplots(2, 1, figsize=(10, 15), sharex=True)
+    fig, axs = plt.subplots(2, 1, figsize=(3, 3), sharex=True)
     df = df.loc[df["lr"] != 0.0]
     for lr, group in df.groupby("lr"):
         for method, method_group in group.groupby("method"):
@@ -49,42 +51,41 @@ def main():
             target_env_state = method_group["env_state"].values[0][:, 0]
             network_env_state = jnp.mean(method_group["env_state"].values[0], axis=1)
 
-            # Smooth the signal
-            target_env_state = jnp.convolve(
-                target_env_state, jnp.ones(1000) / 1000, mode="same"
-            )
             axs[0].plot(
                 ts[1:],
                 target_env_state[1:],
-                label=f"Target Neuron ({method}, lr={lr})",
+                label=f"{method.capitalize()}",
                 color=color,
             )
             axs[0].plot(
                 ts[1:],
                 network_env_state[1:],
-                label=f"Network ({method}, lr={lr})",
                 linestyle="--",
                 color=color,
             )
 
             # Plot mean synaptic weight ot the target neuron and the entire network
-            mean_w = method_group["W_mean"].values[0]
-            target_w = jnp.nanmean(method_group["W_target"].values[0], axis=1)
+            mean_w = method_group["mean_w"].values[0]
+            target_w = method_group["target_w"].values[0]
+            if jnp.any(~jnp.isfinite(target_w)):
+                target_w = jnp.where(jnp.isfinite(target_w), target_w, jnp.nan)
+            mean_w_target = jnp.nanmean(target_w, axis=1)
 
             axs[1].plot(
                 ts[1:],
-                target_w[1:],
-                label=f"Target Neuron Mean Weight ({method}, lr={lr})",
+                mean_w_target[1:],
+                label=f"{method.capitalize()}",
                 color=color,
             )
             axs[1].plot(
                 ts[1:],
                 mean_w[1:],
-                label=f"Network Mean Weight ({method}, lr={lr})",
                 linestyle="--",
                 color=color,
             )
 
+    axs[0].set_ylabel("Firing Rate")
+    axs[1].set_ylabel("Mean Synaptic Weight")
     for ax in axs:
         ax.legend()
 
